de2611158c31f929c429209b7df24c6536e1b587
Danton <dantonmel8@gmail.com>
2025-06-26 12:40:26 -0300

    arrumei o braço do robo

---
+++ TeamCode/src/main/java/OpMode/revcnd.java
@@ -0,0 +1,15 @@
+package OpMode;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@Config
+@TeleOp(name="revcnd")
+public class revcnd extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
